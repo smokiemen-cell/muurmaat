5f3851b422864adf18ffa6b9c0c368611deec43d
smokiemen-cell <smokiemen@gmail.com>
2026-09-24 16:52:37 +0200

    Use installed ARCore version

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -35,5 +35,6 @@ dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.ar:core:1.56.0")
     implementation("com.google.ar.sceneform.ux:sceneform-ux:1.17.1")
 }
